f8f453ff5e0caa7b7c7485443c9abb67426fb579
fanzhiri <48921093@qq.com>
2017-02-18 09:39:47 +0800

    给市场页面添加轮播

--- app/views/Market.js
+++ app/views/Market.js
@@ -5,22 +5,36 @@ import React, { Component } from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import {Actions} from "react-native-router-flux";
 import Button from "react-native-button";
+import Swiper from 'react-native-swiper'
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB',
+    },
 });
 
 class Market extends Component {
     render(){
         return (
             <View style={styles.container}>
-                <Text>Market</Text>
+                <Swiper height={240} loop={true} autoplay={true}>
+                    <View style={styles.slide}>
+                        <Text> abc </Text>
+                    </View>
+                    <View style={styles.slide}>
+                        <Text> def </Text>
+                    </View>
+                    <View style={styles.slide}>
+                        <Text> ghi </Text>
+                    </View>
+                </Swiper>
                 <Button onPress={Actions.pop}>Market</Button>
             </View>
         );
