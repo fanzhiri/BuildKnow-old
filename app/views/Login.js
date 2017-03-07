ca8096e8f106d93c6d999e7ca10a8caa049e2d1e
slakofan <48921093@qq.com>
2017-03-07 19:07:03 +0800

    添加和服务器对接

--- app/views/Login.js
+++ app/views/Login.js
@@ -17,11 +17,53 @@ const styles = StyleSheet.create({
 
 });
 
+var dologinpostUrl = "https://slako.applinzi.com/index.php?m=question&c=api&a=login";
+
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loginresult:"no"
+        };
+        this._dologin = this.dologin.bind(this)
+    }
+
+    dologin(){
+        let formData = new FormData();
+        formData.append("username","fanzhiri");
+        formData.append("password","123456");
+        var opts = {
+            method:"POST",
+            body:formData
+        }
+        fetch(dologinpostUrl,opts)
+            .then((response) => response.json())
+            .then((responseData) => {
+                if(responseData.message =="success"){
+                    this.setState({
+                        loginresult:"ok"
+                    })
+                    Actions.main();
+                }else{
+                    this.setState({
+                        loginresult:responseData.message
+                    })
+                }
+
+            })
+            .catch((error) => {
+                alert(error)
+            })
+    }
+
     render(){
         return (
             <View style={GlobleStyles.withoutTitleContainer}>
-                <Button onPress={()=> Actions.main()}>登录</Button>
+                <Text>{this.state.loginresult}</Text>
+                <Button onPress={this._dologin}>登录</Button>
+                {/*<Button onPress={()=> Actions.main()}>登录</Button>*/}
                 <Button onPress={()=> Actions.register()}>注册</Button>
                 <Button onPress={()=> Actions.introduce()}>介绍</Button>
             </View>
