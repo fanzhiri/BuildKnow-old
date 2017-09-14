dd4591526194fbd548d6e8e7b563a66fa451f46c
slakofan <48921093@qq.com>
2017-09-14 17:39:03 +0800

    完善测试模式

--- app/views/BeginTest.js
+++ app/views/BeginTest.js
@@ -33,19 +33,38 @@ const styles = StyleSheet.create({
 
 });
 
+var doGetBookQuestionUrl = "https://slako.applinzi.com/index.php?m=question&c=personal&a=getbookquestion";
+
 class BeginTest extends Component {
 
     constructor(props) {
         super(props);
 
+        let t_defaultMode={
+            qst_num:8,
+            all_time:2,//min
+            one_time:0,
+            wrong_end:10,
+            pull_back:"yes"
+        };
+
         this.state = {
-            bookquestion_data_source:null,
-            modeselect_idx:0
+            bookquestion_data_source:[null,null,null,null],//每次选中新的未准备好的模式，都要重新生成，
+            modeselect_idx:0,
+            defaultMode:t_defaultMode,
+            testMode:[t_defaultMode,null,null,null],
+            adjust:0,
+            fetching:0
         };
     }
 
-    fetch_bookquestion(){
-
+    fetch_bookquestion(idx){
+        if(this.state.fetching == 1){
+            return;
+        }
+        this.setState({
+            fetching:1
+        });
         let formData = new FormData();
         formData.append("auth",global.auth);
         formData.append("bookid",this.props.bookdata.question_book_id);
@@ -54,43 +73,65 @@ class BeginTest extends Component {
             method:"POST",
             body:formData
         }
-        fetch(doGetMyBookQuestionUrl,opts)
+        fetch(doGetBookQuestionUrl,opts)
             .then((response) => response.json())
             .then((responseData) => {
                 if(responseData.code == 100){
+                    let t_bookquestion_data_source=this.state.bookquestion_data_source;
+                    t_bookquestion_data_source[this.state.modeselect_idx]=responseData.data;
                     this.setState({
-                        bookquestion_data_source:responseData.data
+                        bookquestion_data_source:t_bookquestion_data_source,
+                        fetching:0
                     })
                 }else{
                     alert(responseData.message);
+                    this.setState({
+                        fetching:0
+                    });
                 }
 
             })
             .catch((error) => {
-                alert(error)
+                alert(error);
+                this.setState({
+                    fetching:0
+                });
             })
     }
 
-    handleBeginTest() {
-        Actions.begintest({intype:0,bookdata:this.state.bookdata});
-    }
-
     onModePressFunc(idx){
+        if(this.state.adjust == 1){
+            return;
+        }
+        if(this.state.testMode[idx] == null){
+            return;
+        }
         this.setState({
             modeselect_idx:idx
         });
     }
 
-    onModeAdjustFunc(){
-
+    onModeAdjustFunc(idx){
+        if(this.state.modeselect_idx == idx){
+            this.setState({
+                adjust:(this.state.adjust==1)?0:1
+            });
+        }
     }
 
     renderadjust(idx){
         let adjusttext="";
         let select_background_collor = "#FFFFFF";
+        if(this.state.testMode[idx] == null){
+            adjusttext="添加";
+        }
         if(this.state.modeselect_idx == idx){
             adjusttext="调整";
             select_background_collor="#FF8000";
+            if(this.state.adjust == 1){
+                adjusttext="保存";
+                select_background_collor="#8EF9E0";
+            }
         }
             return(
                 <View style={{flex:1,justifyContent:"center",alignItems:"center",backgroundColor:select_background_collor}}>
@@ -111,6 +152,19 @@ class BeginTest extends Component {
             select_this= "选中"+idx;
             select_background_collor="#FF8000";
         }
+        let nowTestMode =this.state.testMode[idx];
+        let t_defaultMode={
+            qst_num:"",
+            all_time:"",//min
+            one_time:"",
+            wrong_end:"",
+            pull_back:""
+        };
+
+
+        if(nowTestMode == null){
+            nowTestMode = t_defaultMode;
+        }
         return(
             <View style={{flex:1,backgroundColor:select_background_collor}}>
                 <TouchableOpacity onPress={()=> this.onModePressFunc(idx)} activeOpacity={0.8}>
@@ -119,19 +173,19 @@ class BeginTest extends Component {
                     </View>
                 </TouchableOpacity>
                 <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-                    <Text>1</Text>
+                    <Text>{nowTestMode.qst_num}</Text>
                 </View>
                 <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-                    <Text>2</Text>
+                    <Text>{nowTestMode.all_time}</Text>
                 </View>
                 <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-                    <Text>3</Text>
+                    <Text>{nowTestMode.one_time}</Text>
                 </View>
                 <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-                    <Text>4</Text>
+                    <Text>{nowTestMode.wrong_end}</Text>
                 </View>
                 <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-                    <Text>5</Text>
+                    <Text>{nowTestMode.pull_back}</Text>
                 </View>
 
                 {this.renderadjust(idx)}
@@ -141,17 +195,73 @@ class BeginTest extends Component {
     }
     renderModeTitleItem(title){
         return(
-            <View style={{flex:1,justifyContent:"center"}}>
-                <Text style={{fontSize:18}}>{title}</Text>
+            <View style={{flex:1.6,justifyContent:"center"}}>
+                <Text style={{fontSize:16}}>{title}</Text>
             </View>
         )
     }
+
+    handleBeginCommonTest(){
+        let t_questiondataarr = JSON.parse(this.props.bookdata.qidtext);
+        //let t_questiondataarr = this.state.bookquestion_data_source[this.state.modeselect_idx];
+        let nowTestMode = this.state.testMode[this.state.modeselect_idx];
+        t_questiondataarr = t_questiondataarr.reverse();
+        t_questiondataarr = t_questiondataarr.slice(0,nowTestMode.qst_num);
+        let answer_arr = new Array();
+        for(let i=0;i < nowTestMode.qst_num;i++){
+            let rightindex = Math.floor(Math.random() * 4);
+
+            let t_questiondata = t_questiondataarr[i];
+            let answerarr=JSON.parse(t_questiondata.wrong_answer);
+            let rightanswer=t_questiondata.right_answer;
+            answerarr.splice(rightindex,0,rightanswer);
+            answer_arr.push({ri:rightindex,randomanswer:answerarr});
+        }
+
+        Actions.answerquestion({
+            intype:2,
+            readyquestion_arr:t_questiondataarr,
+            answer_arr:answer_arr,
+            answermode:1});
+    }
+
+    renderNextButton(){
+        /*
+        if(this.state.bookquestion_data_source[this.state.modeselect_idx] == null){
+            let buttontext="生成试卷";
+            if(this.state.fetching == 1){
+                buttontext="生成中。。。";
+            }
+            return(
+                <TouchableOpacity onPress={ () =>this.fetch_bookquestion()} activeOpacity={0.8}>
+                    <View style={[styles.beginButton,{backgroundColor: '#D09E30'}]}  >
+                        <Text style={{fontSize: 18}}>
+                            {buttontext}
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            )
+        }else{
+        */
+            return(
+                <TouchableOpacity onPress={ () =>this.handleBeginCommonTest()} activeOpacity={0.8}>
+                    <View style={styles.beginButton}  >
+                        <Text style={{fontSize: 18}}>
+                            开始测验
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            )
+        //}
+
+    }
+
     rendercommontest(){
         return(
             <View style={{flex:1}}>
                 <Text style={styles.textdesc}>名字：{this.props.bookdata.bookname}</Text>
                 <Text style={styles.textdesc}>总题数：{this.props.bookdata.questioncount}</Text>
-                <View style={{flex:1,flexDirection:"row",height:108,margin:16,backgroundColor:"#FFFFFF"}}>
+                <View style={{flex:1,flexDirection:"row",height:108,margin:16,padding:8,backgroundColor:"#FFFFFF"}}>
                     <View style={{flex:2}}>
                         <View style={{height:32}}></View>
                         {this.renderModeTitleItem("限定题数：")}
@@ -168,18 +278,15 @@ class BeginTest extends Component {
                 </View>
 
                 <View style={styles.buttonContainer}>
-                    <View style={styles.beginButton}  onPress={ () =>this.handleBeginTest()}>
-                        <Text style={{fontSize: 18}}>
-                            开始测验
-                        </Text>
-                    </View>
+                    {this.renderNextButton()}
                 </View>
             </View>
         )
     }
 
-    goingtopktest(){
+    handleBeginTest(){
         let t_questiondataarr = JSON.parse(this.props.pkdata.questionsready);
+
         let answer_arr = new Array();
         for(let i=0;i < this.props.pkdata.testnum;i++){
             let rightindex = Math.floor(Math.random() * 4);
@@ -205,11 +312,13 @@ class BeginTest extends Component {
                 <Text style={styles.textdesc}>参与人数：{pkpeople.length}</Text>
                 <Text style={styles.textdesc}>截止时间：{this.props.pkdata.overdue}</Text>
                 <View style={styles.buttonContainer}>
-                    <View style={styles.beginButton}  onPress={ () =>this.goingtopktest()}>
-                        <Text style={{fontSize: 18}}>
-                            开始测验
-                        </Text>
-                    </View>
+                    <TouchableOpacity onPress={ () =>this.handleBeginTest()} activeOpacity={0.8}>
+                        <View style={styles.beginButton}  >
+                            <Text style={{fontSize: 18}}>
+                                开始测验
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         )
