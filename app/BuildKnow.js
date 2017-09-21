fdccc0eb91bbd5ba833d5e190f12cd1d0c384f51
slakofan <48921093@qq.com>
2017-09-21 16:59:56 +0800

    MyCollectBookList改MyCollectList

--- app/BuildKnow.js
+++ app/BuildKnow.js
@@ -70,7 +70,7 @@ import RegisterVerify from './views/RegisterVerify'
 import FeedBack from './views/FeedBack'
 import ReviewCheckList from './views/ReviewCheckList'
 import ReviewQuestion from './views/ReviewQuestion'
-import MyCollectBookList from './views/MyCollectBookList'
+import MyCollectList from './views/MyCollectList'
 import ChatList from './views/ChatList'
 //51
 import TestResult from './views/TestResult'
@@ -175,7 +175,7 @@ export default class BuildKnow extends Component {
                 <Scene key="feedback"           title="意见反馈" component={FeedBack} duration={0} />
                 <Scene key="reviewchecklist"    title="审核表" component={ReviewCheckList} duration={0} />
                 <Scene key="reviewquestion"     title="审核题目" component={ReviewQuestion} duration={0} />
-                <Scene key="mycollectbooklist"  title="收藏题本" component={MyCollectBookList} duration={0} />
+                <Scene key="mycollectlist"      title="我的收藏" component={MyCollectList} duration={0} />
                 <Scene key="chatlist"           title="聊天" component={ChatList} duration={0} />
                 <Scene key="testresult"         title="测试结果" component={TestResult} duration={0}  />
                 <Scene key="answerlibedit"      title="答案库编辑" component={AnswerLibEdit} duration={0}  />
