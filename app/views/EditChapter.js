f67982bb96f9feac38512f7fe705f4a373b7146e
slakofan <48921093@qq.com>
2017-09-29 17:31:26 +0800

    完成章节编辑

--- app/views/EditChapter.js
+++ app/views/EditChapter.js
@@ -67,9 +67,15 @@ const styles = StyleSheet.create({
 });
 
 var doGetMyBooksUrl = "https://slako.applinzi.com/index.php?m=question&c=personal&a=getmybooks";
+
+var doSetChapterUrl = "https://slako.applinzi.com/index.php?m=question&c=personal&a=setchapter";
+
 var httpsBaseUrl = "https://slako.applinzi.com/";
 
+const MAX_CHAPTER =32 ;
+
 class EditChapter extends Component {
+
 
     constructor(props) {
 
@@ -79,7 +85,7 @@ class EditChapter extends Component {
             chaterlen=props.chapter_arr.length;
         }
 
-        let t_remainadd= 20 - chaterlen;//最多20章节
+        let t_remainadd= MAX_CHAPTER - chaterlen;//最多20章节
 
         let t_chapterList=new Array();
         for(let i=0;i < chaterlen;i++){
@@ -88,17 +94,23 @@ class EditChapter extends Component {
                 idx:props.chapter_arr[i].idx,
                 sortnum:props.chapter_arr[i].sortnum,
                 qstnum:props.chapter_arr[i].qstnum,
+                num_error:0,
+                name_error:0
             }
             t_chapterList[i]=chapteritemone;
+
         }
+
+
+        let t_chapterListShadow = JSON.parse(JSON.stringify(t_chapterList));
 
         this.state = {
             chapter_arr: props.chapter_arr,
             gorefreshing:false,
             editing:false,
             inputtextstring:"",
-            remainadd:t_remainadd,
-            chapterList:t_chapterList
+            chapterList:t_chapterList,
+            chapterListShadow:t_chapterListShadow,
         };
 
         this._renderChapterItem = this.renderChapterItem.bind(this)
@@ -131,6 +143,36 @@ class EditChapter extends Component {
             })
     }
 
+    dofetch_setChapter(t_chapterListShadow){
+
+        let formData = new FormData();
+        formData.append("auth",global.auth);
+        formData.append("userid",global.userid);
+        formData.append("bookid",this.props.bookid);
+        formData.append("chapter",JSON.stringify(t_chapterListShadow));
+        var opts = {
+            method:"POST",
+            body:formData
+        }
+        fetch(doSetChapterUrl,opts)
+            .then((response) => response.json())
+            .then((responseData) => {
+                if(responseData.code == 100){
+                    this.setState({
+                        chapterList:t_chapterListShadow,
+                        editing:false
+                    })
+
+                }else{
+                    alert(responseData.message);
+                }
+
+            })
+            .catch((error) => {
+                alert(error)
+            })
+    }
+
     renderMyBooksView(){
         if(this.state.books_data_source ==null){
             this.dofetch_mybooks();
@@ -143,32 +185,167 @@ class EditChapter extends Component {
     onListItemClick(rowData, sectionID, rowID){
 
     }
-    dellChapterItem(){
+    dellChapterItem(rowID){
+        let t_chapterListShadow=this.state.chapterListShadow;
+        t_chapterListShadow.splice(rowID,1);
 
+        let chapterlen = t_chapterListShadow.length;
+        //检查排序是否有重复 begin
+        let numover =false;
+        for(let i=0;i<chapterlen;i++){
+            if(parseInt(t_chapterListShadow[i].sortnum)>chapterlen){
+                t_chapterListShadow[i].num_error = 2;
+                numover =true;
+            }
+        }
+
+        if(numover == false){
+            for(let i=0;i<chapterlen;i++){
+                for(let j=0;j<chapterlen;j++){
+                    if(i == j)continue;
+                    if(parseInt(t_chapterListShadow[i].sortnum) == parseInt(t_chapterListShadow[j].sortnum)){
+                        t_chapterListShadow[i].num_error = 2;
+                        t_chapterListShadow[j].num_error = 2;
+                        break;
+                    }
+                    if(j ==(chapterlen-1)){
+                        t_chapterListShadow[i].num_error = 0;
+                    }
+                    if(i==(chapterlen-1) && (j==i-1)){
+                        t_chapterListShadow[i].num_error = 0;
+                    }
+                }
+            }
+        }
+        //检查排序是否有重复 end
+        //检查名字是否有重复 begin
+        for(let i=0;i<chapterlen;i++){
+            for(let j=0;j<chapterlen;j++){
+                if(i == j)continue;
+                if(t_chapterListShadow[i].name == t_chapterListShadow[j].name){
+                    t_chapterListShadow[i].name_error = 1;
+                    t_chapterListShadow[j].name_error = 1;
+                    break;
+                }
+                if(j ==(chapterlen-1)){
+                    t_chapterListShadow[i].name_error = 0;
+                }
+                if(i==(chapterlen-1) && (j==i-1)){
+                    t_chapterListShadow[i].name_error = 0;
+                }
+            }
+        }
+        //检查名字是否有重复 end
+        this.setState({
+            chapterListShadow:t_chapterListShadow
+        })
+    }
+
+    sortNumChange(rowID,text){
+        let t_chapterListShadow = this.state.chapterListShadow;
+        t_chapterListShadow[rowID].sortnum = text;
+
+        let t_sortnum = parseInt(text);
+        if(t_sortnum < 1 || t_sortnum > t_chapterListShadow.length){
+            t_chapterListShadow[rowID].num_error =1;
+        }else{
+            t_chapterListShadow[rowID].num_error =0;
+        }
+
+        let chapterlen = t_chapterListShadow.length;
+        if(t_chapterListShadow[rowID].num_error == 0 ){
+            for(let i=0;i<chapterlen;i++){
+                for(let j=0;j<chapterlen;j++){
+                    if(i == j)continue;
+                    if(parseInt(t_chapterListShadow[i].sortnum) == parseInt(t_chapterListShadow[j].sortnum)){
+                        t_chapterListShadow[i].num_error = 2;
+                        t_chapterListShadow[j].num_error = 2;
+                        break;
+                    }
+                    if(j ==(chapterlen-1)){
+                        t_chapterListShadow[i].num_error = 0;
+                    }
+                    if(i==(chapterlen-1) && (j==i-1)){
+                        t_chapterListShadow[i].num_error = 0;
+                    }
+                }
+            }
+        }
+
+        this.setState({
+            chapterListShadow:t_chapterListShadow
+        })
+    }
+
+    nameChange(rowID,text){
+        let t_chapterListShadow = this.state.chapterListShadow;
+        t_chapterListShadow[rowID].name = text;
+
+        let chapterlen = t_chapterListShadow.length;
+
+        for(let i=0;i<chapterlen;i++){
+            if(t_chapterListShadow[i].name ==''){
+                t_chapterListShadow[i].name_error = 1;
+                break;
+            }
+            for(let j=0;j<chapterlen;j++){
+                if(i == j)continue;
+                if(t_chapterListShadow[i].name == t_chapterListShadow[j].name){
+                    t_chapterListShadow[i].name_error = 1;
+                    t_chapterListShadow[j].name_error = 1;
+                    break;
+                }
+                if(j ==(chapterlen-1)){
+                    t_chapterListShadow[i].name_error = 0;
+                }
+                if(i==(chapterlen-1) && (j==i-1)){
+                    t_chapterListShadow[i].name_error = 0;
+                }
+            }
+        }
+
+
+        this.setState({
+            chapterListShadow:t_chapterListShadow
+        })
     }
 
     renderAddChapterItem(rowData, sectionID, rowID){
+        if(this.state.chapterListShadow.length == 0){
+            return null;
+        }
+
+        let formcolor = "#04c4c4"
+        if(this.state.chapterListShadow[rowID].num_error == 1){
+            formcolor = "#FF0000"
+        }else if(this.state.chapterListShadow[rowID].num_error == 2){
+            formcolor = "#0000FF"
+        }
+        let formNamecolor = "#04c4c4"
+        if(this.state.chapterListShadow[rowID].name_error == 1){
+            formNamecolor = "#0000FF"
+        }
         return(
-            <View>
+            <View style={styles.listItem}>
                 <TextInput
-                    style={{width:48,fontSize:16}}
-                    onChangeText={(text) => this.setState({inputtextstring:text})}
-                    value={this.state.chapterList[rowID].sortnum}
+                    style={{width:32,fontSize:14,borderColor:formcolor,borderWidth: 2,padding:2,marginLeft:6}}
+                    onChangeText={(text) => this.sortNumChange(rowID,text)}
+                    value={this.state.chapterListShadow[rowID].sortnum.toString()}
                     placeholder={""}
                     maxLength={2}
                     multiline={false}
                 />
                 <TextInput
-                    style={{width:180,fontSize:16}}
-                    onChangeText={(text) => this.setState({inputtextstring:text})}
-                    value={this.state.chapterList[rowID].name}
-                    placeholder={""}
+                    style={{width:100,fontSize:14,borderColor: formNamecolor,borderWidth: 2,padding:2,marginLeft:10}}
+                    onChangeText={(text) => this.nameChange(rowID,text)}
+                    value={this.state.chapterListShadow[rowID].name}
+                    placeholder={"填写"}
                     maxLength={20}
                     multiline={false}
                 />
                 <View style={{flex:1,flexDirection:"row",justifyContent:"flex-end",alignItems:"center"}}>
-                    <TouchableOpacity onPress={() => this.dellChapterItem()}>
-                        <View style={{width:48,height:28,marginLeft:8,alignItems:"center",justifyContent:"center",backgroundColor:"#00FF00",borderRadius:6}}>
+                    <TouchableOpacity onPress={() => this.dellChapterItem(rowID)}>
+                        <View style={{width:48,height:28,marginLeft:8,alignItems:"center",justifyContent:"center",backgroundColor:"#FF0F00",borderRadius:6}}>
                             <Text >删除</Text>
                         </View>
                     </TouchableOpacity>
@@ -176,8 +353,14 @@ class EditChapter extends Component {
             </View>
         )
     }
-
-    renderChapterItem(rowData, sectionID, rowID){
+    renderChapterItem(rowData, sectionID, rowID) {
+        if(this.state.editing){
+            return this.renderAddChapterItem(rowData, sectionID, rowID);
+        }else {
+            return this.renderChapterNormalItem(rowData, sectionID, rowID);
+        }
+    }
+    renderChapterNormalItem(rowData, sectionID, rowID){
 
         return (
             <TouchableOpacity onPress={() => this.onListItemClick(rowData, sectionID, rowID)}>
@@ -193,12 +376,32 @@ class EditChapter extends Component {
     }
 
     beginAdd(){
+        let t_chapterList = JSON.parse(JSON.stringify(this.state.chapterList));
         this.setState({
+            chapterListShadow:t_chapterList,
             editing:true
         })
+
     }
 
-    addChapter(){
+    saveChapter(){
+
+        let t_chapterListShadow = JSON.parse(JSON.stringify(this.state.chapterListShadow));
+        let chapterlen = t_chapterListShadow.length;
+
+        for(let i=0;i<chapterlen;i++){
+            if(t_chapterListShadow[i].name_error != 0){
+                return;
+            }else if(t_chapterListShadow[i].num_error != 0){
+                return;
+            }
+        }
+        t_chapterListShadow.sort(function (a,b) {
+            let aNum =parseInt(a.sortnum);
+            let bNum =parseInt(b.sortnum);
+            return aNum >= bNum?1:-1;
+        });
+        this.dofetch_setChapter(t_chapterListShadow);
 
     }
 
@@ -211,21 +414,43 @@ class EditChapter extends Component {
 
 
     addChapterItem(){
-        let t_chapterList=this.state.chapterList;
+        let remainadd = MAX_CHAPTER-this.state.chapterListShadow.length;
+        let t_chapterListShadow=this.state.chapterListShadow;
         let chapteritemone ={
-            name:"填写",
+            name:"",
             idx:0,
-            sortnum:20-this.state.remainadd+1,
+            sortnum:MAX_CHAPTER-remainadd+1,
             qstnum:0,
+            num_error:0,
+            name_error:1
         }
-        t_chapterList.push(chapteritemone);
+        t_chapterListShadow.push(chapteritemone);
         this.setState({
-            chapterList:t_chapterList
+            chapterListShadow:t_chapterListShadow
         })
+    }
+
+    renderAddChapterButton(){
+        if(this.state.chapterList.length >= 32){
+            return;
+        }else{
+            let remainadd = MAX_CHAPTER-this.state.chapterListShadow.length;
+            return(
+                <View style={styles.listItem}>
+                    <TouchableOpacity onPress={() => this.addChapterItem()}>
+                        <View style={{width:48,height:28,marginLeft:8,alignItems:"center",justifyContent:"center",backgroundColor:"#00FF00",borderRadius:6}}>
+                            <Text >添加</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <Text style={{marginLeft:12}}>还可以添加{remainadd}个章节</Text>
+                </View>
+            )
+        }
     }
 
     renderAddView(){
         if(this.state.editing == false){
+            let remainadd = MAX_CHAPTER-this.state.chapterList.length;
             return(
                 <View style={styles.listItem}>
                     <TouchableOpacity onPress={() => this.beginAdd()}>
@@ -233,22 +458,15 @@ class EditChapter extends Component {
                             <Text >编辑</Text>
                         </View>
                     </TouchableOpacity>
-                    <Text style={{marginLeft:12}}>还可以添加{this.state.remainadd}个章节</Text>
+                    <Text style={{marginLeft:12}}>还可以添加{remainadd}个章节</Text>
                 </View>
             )
         }else{
             return(
             <View>
+                {this.renderAddChapterButton()}
                 <View style={styles.listItem}>
-                    <TouchableOpacity onPress={() => this.addChapterItem()}>
-                        <View style={{width:48,height:28,marginLeft:8,alignItems:"center",justifyContent:"center",backgroundColor:"#00FF00",borderRadius:6}}>
-                            <Text >添加</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <Text style={{marginLeft:12}}>还可以添加{this.state.remainadd}个章节</Text>
-                </View>
-                <View style={styles.listItem}>
-                    <TouchableOpacity onPress={() => this.addChapter()}>
+                    <TouchableOpacity onPress={() => this.saveChapter()}>
                         <View style={{width:48,height:28,marginLeft:8,alignItems:"center",justifyContent:"center",backgroundColor:"#00FF00",borderRadius:6}}>
                             <Text >保存</Text>
                         </View>
@@ -267,13 +485,15 @@ class EditChapter extends Component {
     }
 
     renderChapterView(){
+        let nowChapterList = this.state.editing?this.state.chapterListShadow:this.state.chapterList;
+
         return (
             <ScrollView
                 style={styles.scrlist}>
                 <ListView
                     ref="scrview"
                     style={styles.list}
-                    dataSource={DataStore.cloneWithRows(this.state.chapterList)}
+                    dataSource={DataStore.cloneWithRows(nowChapterList)}
                     renderRow={this._renderChapterItem}
                     enableEmptySections = {true}
                 />
